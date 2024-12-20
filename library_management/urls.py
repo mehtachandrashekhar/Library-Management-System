0dc9bf3dc2bc37636ac42bd829356819b35066f3
mehtachandrashekhar <mehtachandrashekhar84@gmail.com>
2024-12-20 07:19:48 +0530

    fix: home url

--- library_management/urls.py
+++ library_management/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.contrib import admin
+
+def home(request):
+    return HttpResponse("Welcome to the Library Management System")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('library.urls')),
+    path('', home, name='home'),
 ]
